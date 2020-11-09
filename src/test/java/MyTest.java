8b19bb9e3c8627f1d1c339b18802f5b746fcc417
Andrew Strain <andrew.d.strain@outlook.com>
2020-11-09 10:33:55 -0500

    Demo from lecture

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    void test() {
+        Assertions.fail("not implemented yet");
+    }
+}
