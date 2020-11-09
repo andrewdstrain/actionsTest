5e5d3a4797f745421824935062052e4e656c621f
Andrew Strain <andrew.d.strain@outlook.com>
2020-11-09 10:45:54 -0500

    changes

---
+++ src/test/java/OtherTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OtherTest {
+    @Test
+    void test() {
+        Assertions.fail("not implemented yet");
+//        Assertions.assertEquals(5, 5);
+    }
+}
